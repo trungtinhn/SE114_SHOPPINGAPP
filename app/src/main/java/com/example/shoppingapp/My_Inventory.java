c7f79045820fc1cd8cef1da0715f1ad0f86ca65b
Le Khanh Duy <21521996@gm.uit.edu.vn>
2023-05-27 16:25:31 +0700

    commit test

---
+++ app/src/main/java/com/example/shoppingapp/My_Inventory.java
@@ -0,0 +1,70 @@
+package com.example.shoppingapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+
+public class My_Inventory extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private RecyclerView recyclerView1;
+    private ArrayList<product_object> arrayList= new ArrayList<>();
+    private My_inventory_Adapter adapter;
+
+    public My_Inventory() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_my__inventory, container, false);
+        recyclerView1= view.findViewById(R.id.RCV_My_inventory);
+        setdata();
+        return view;
+    }
+
+    private void setdata() {
+        arrayList= new ArrayList<>();
+        arrayList.add(new product_object("T-Shirt Black Blank-VSD343545D - New Elevent",139.999,R.drawable.anh1,20,20,20,20,1));
+        arrayList.add(new product_object("T-Shirt Black Blank-VSD3435231 - New Elevent",139.909,R.drawable.anh1,20,20,20,20,1));
+        arrayList.add(new product_object("T-Shirt Black Blank-VSD343523d - New Elevent",139.9009,R.drawable.anh1,20,20,20,20,1));
+        adapter = new My_inventory_Adapter(getActivity());
+        adapter.setData(arrayList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity()  , RecyclerView.VERTICAL, false);
+        recyclerView1.setLayoutManager(layoutManager);
+        recyclerView1.setAdapter(adapter);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
---
+++ app/src/main/java/com/example/shoppingapp/user_object.java
@@ -0,0 +1,47 @@
+package com.example.shoppingapp;
+
+public class user_object {
+    private String name;
+    private String ID;
+    private double moneytotal;
+    private int ava;
+
+    public int getAva() {
+        return ava;
+    }
+
+    public void setAva(int ava) {
+        this.ava = ava;
+    }
+
+    public user_object(String name, String ID, double moneytotal, int ava) {
+        this.name = name;
+        this.ID = ID;
+        this.moneytotal = moneytotal;
+        this.ava = ava;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public double getMoneytotal() {
+        return moneytotal;
+    }
+
+    public void setMoneytotal(double moneytotal) {
+        this.moneytotal = moneytotal;
+    }
+}
