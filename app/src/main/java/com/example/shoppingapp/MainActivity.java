a11f3b8b0eb9bbff49444a429931121e3cf142ad
TSang2202 <21522546.gm.uit.edu.vn>
2023-06-01 13:09:18 +0700

    update Customer View

--- app/src/main/java/com/example/shoppingapp/MainActivity.java
+++ app/src/main/java/com/example/shoppingapp/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.shoppingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.shoppingapp.customerview.BottomNavigationCustomActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnOpen;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        btnOpen = findViewById(R.id.btnOpen);
+
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BottomNavigationCustomActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
