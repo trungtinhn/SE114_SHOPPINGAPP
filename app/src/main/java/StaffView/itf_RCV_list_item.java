9eecb5b87a7a12c202dfc4ead049b41469a4e7a6
Tính Nguyễn Trung <21520115@ms.uit.edu.vn>
2023-06-09 22:49:14 +0700

    MergeFixCode

--- app/src/main/java/StaffView/itf_RCV_list_item.java
+++
@@ -1,5 +0,0 @@
-package StaffView;
-
-public interface itf_RCV_list_item {
-    void onClick(item_object item);
-}
---
+++ app/src/main/java/com/example/shoppingapp/StaffView/itf_RCV_list_item.java
@@ -0,0 +1,5 @@
+package com.example.shoppingapp.StaffView;
+
+public interface itf_RCV_list_item {
+    void onClick(item_object item);
+}
