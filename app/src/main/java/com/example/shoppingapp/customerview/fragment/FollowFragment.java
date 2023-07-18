f1061eb88e790b62707c617f8d880962e371da61
TSang2202 <21522546.gm.uit.edu.vn>
2023-07-18 14:49:20 +0700

    finish YeuThich

--- app/src/main/java/com/example/shoppingapp/customerview/fragment/FollowFragment.java
+++ app/src/main/java/com/example/shoppingapp/customerview/fragment/FollowFragment.java
@@ -1,65 +1,154 @@
 package com.example.shoppingapp.customerview.fragment;
 
+import static android.content.ContentValues.TAG;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.shoppingapp.R;
+import com.example.shoppingapp.customerview.BottomNavigationCustomActivity;
+import com.example.shoppingapp.customerview.product.Product;
+import com.example.shoppingapp.customerview.product.ProductAdapter;
+import com.example.shoppingapp.customerview.product.customer_interface.IClickItemProductListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FollowFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class FollowFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView rcvFollow;
+    private TextView btnEmpty;
+    private RelativeLayout layoutEmpty;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private ProductAdapter productAdapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private BottomNavigationCustomActivity bottomNavigationCustomActivity;
 
-    public FollowFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NotificationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FollowFragment newInstance(String param1, String param2) {
-        FollowFragment fragment = new FollowFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_follow, container, false);
+        View view = inflater.inflate(R.layout.fragment_follow, container, false);
+
+        rcvFollow = view.findViewById(R.id.rcvFollow);
+        btnEmpty = view.findViewById(R.id.btnEmpty);
+        layoutEmpty = view.findViewById(R.id.layoutEmpty);
+        bottomNavigationCustomActivity = (BottomNavigationCustomActivity) getActivity();
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 2);
+        rcvFollow.setLayoutManager(gridLayoutManager);
+
+        getFirebase();
+        setFollow();
+
+        return view;
+    }
+
+    private void getFirebase() {
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+    }
+
+    private void onClickGoToDetailProduct(Product product) {
+        bottomNavigationCustomActivity.gotoDetailProduct(product);
+    }
+
+    private void setFollow() {
+        productAdapter = new ProductAdapter();
+
+        List<Product> products = new ArrayList<>();
+
+        firebaseFirestore.collection("YEUTHICH")
+                .whereEqualTo("MaND", firebaseUser.getUid())
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        if (value.size() == 0) {
+                            layoutEmpty.setVisibility(View.VISIBLE);
+                        }else {
+                            layoutEmpty.setVisibility(View.GONE);
+
+
+                            for (QueryDocumentSnapshot doc : value) {
+                                String maSP = doc.getString("MaSP");
+
+
+                                final DocumentReference docRef = firebaseFirestore.collection("SANPHAM").document(maSP);
+                                docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                                        @Nullable FirebaseFirestoreException e) {
+                                        if (e != null) {
+                                            Log.w(TAG, "Listen failed.", e);
+                                            return;
+                                        }
+
+                                        if (snapshot != null && snapshot.exists()) {
+                                            Log.d(TAG, "Current data: " + snapshot.getData());
+                                            String masp = snapshot.getString("MaSP");
+                                            String name = snapshot.getString("TenSP");
+                                            Long giaSP = snapshot.getLong("GiaSP");
+                                            List<String> Anh = (List<String>) snapshot.get("HinhAnhSP");
+                                            products.add(new Product(Anh.get(0), name, masp, giaSP));
+
+                                            productAdapter.setData(products, new IClickItemProductListener() {
+                                                @Override
+                                                public void onClickItemProduct(Product product) {
+                                                    onClickGoToDetailProduct(product);
+                                                }
+                                            });
+                                            rcvFollow.setAdapter(productAdapter);
+                                        } else {
+                                            Log.d(TAG, "Current data: null");
+                                        }
+                                    }
+                                });
+                            }
+
+                        }
+
+
+                    }
+                });
     }
 }
