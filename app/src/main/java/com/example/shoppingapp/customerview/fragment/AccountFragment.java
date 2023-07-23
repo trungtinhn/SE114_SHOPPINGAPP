d6dd395ca793b1989b6eae1a88f52a70a0dc7d2e
Le Khanh Duy <21521996@gm.uit.edu.vn>
2023-07-23 17:14:12 +0700

    update

--- app/src/main/java/com/example/shoppingapp/customerview/fragment/AccountFragment.java
+++ app/src/main/java/com/example/shoppingapp/customerview/fragment/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.example.shoppingapp.customerview.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,54 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.shoppingapp.R;
+import com.example.shoppingapp.customerview.activity.OrderActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AccountFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AccountFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private LinearLayout YourOrder;
+    private LinearLayout Changepass;
+    private LinearLayout Help;
+    private LinearLayout Profile;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public AccountFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AccountFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AccountFragment newInstance(String param1, String param2) {
-        AccountFragment fragment = new AccountFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        YourOrder = view.findViewById(R.id.l1);
+        Changepass = view.findViewById(R.id.l4);
+        Help = view.findViewById(R.id.l2);
+        Profile = view.findViewById(R.id.l3);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+//        YourOrder.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(, AccountFragment.class);
+//                startActivity(intent);
+//            }
+//        });
+//        return view;
+//    }
+
+
+        return view;
     }
 }
